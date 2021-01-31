762cb0372857e585ba25661207b87a69220470ed
Rameez Qazi <rameez.qazi23@gmail.com>
2021-01-31 21:07:17 +0500

    add some logic on the based of users sctivity when they are sender or recipient the bubble shows accordingly on the based of UI and custom styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
-const App = ()=>{
-    return(
-        <ChatEngine 
-        height = "100vh"
-        projectID = "35960980-665e-4c3f-b1eb-5fe098a410f4"
-        userName = "rameezqazi"
-        userSecret = "123456"
 
-        
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="35960980-665e-4c3f-b1eb-5fe098a410f4"
+            userName="rameezqazi"
+            userSecret="123456"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+
+
         />
     );
 }
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,61 @@
+import MyMessage from './MyMessage';
+import MessageForm from './MessageForm';
+import TheirMessage from './TheirMessage'
+
+const ChatFeed = (props) => {
+
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat]
+
+    console.log("chats==>", chat)
+    console.log("userName==>", userName)
+    console.log("messages==>", messages)
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages)
+        console.log(keys)
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.username;
+            return (
+                <div key={`msg_${index}`} style={{ width: '100%' }}>
+                    <div className='message-block'>
+                        {
+                            isMyMessage
+                                ? <MyMessage message={message} />
+                                : <TheirMessage message={message} lastMessage={message[lastMessageKey]} />
+
+                        }
+                    </div>
+                    <div className='read-receipts' style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+                        Meassages
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    // renderMessages()
+    if(!chat) return<div />
+    return (
+        <div className='chat-feed'>
+            <div className='chat-title-container'>
+                <div className='chat-title'>{chat?.title}</div>
+                <div className='chat-subtitle'>
+                    {chat.people.map((person) => `${person.person.username}`)}
+                </div>
+
+            </div>
+            {renderMessages()}
+            <div style={{ height: '100px' }} />
+            <div className='message-form-container'>
+                <MessageForm {...props} chatID={activeChat} />
+
+            </div>
+        </div>
+    )
+}
+
+export default ChatFeed;
