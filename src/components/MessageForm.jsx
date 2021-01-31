7efaa34cde98a5fa0ba980f0090fa2abd2b94911
Rameez Qazi <rameez.qazi23@gmail.com>
2021-01-31 21:58:00 +0500

    add message bar type a message into form type method with sending parameters

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
+
 const MessageForm = () => {
+    const [value, setValue] = useState('')
+
+    const handleChange = () => {
+
+    }
+    const handleSubmit = () => {
+
+    }
     return (
-        <div>Message Form</div>
+        <form className='message-form' onSubmit={handleSubmit}>
+            <input
+                className='message-input'
+                placeholder='Seend a message...'
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+
+        </form>
     )
 }
 
