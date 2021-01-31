c2c978b0f3ce56990f7a9f1bae578ab128852829
Rameez Qazi <rameez.qazi23@gmail.com>
2021-01-31 21:23:00 +0500

    customize the sender message bubble if is there are any messages or attachments

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,6 +1,31 @@
-const TheirMessage = () => {
+const TheirMessage = ({ message, lastMessage }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username
     return (
-        <div>Their Message</div>
+        <div className='message-row'>
+            {isFirstMessageByUser & (
+                <div
+                    className='message-avatar'
+                    style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+                />
+            )}
+
+            {message?.attachments?.length > 0
+                ? (
+                    <img
+                        src={message.attachments[0].file}
+                        alt='message-attachments'
+                        className='message-image'
+                        style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                    />
+                ) : (
+                    <div className='message' style={{ float: 'left', marginLeft: isFirstMessageByUser ? '4px' : '48px', backgroundColor: '#CABCDC' }}>
+                        {message.text}
+                    </div>
+                )
+
+            }
+        </div>
+
     )
 }
 
