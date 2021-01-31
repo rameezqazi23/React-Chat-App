02be578962b068707d635b6c1cfd2e6f5e081e27
Rameez Qazi <rameez.qazi23@gmail.com>
2021-01-31 18:48:21 +0500

    demo chat render successfully now i am going it to fully customizable

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
-import {ChatEngine} from 'react-chat-engine';
+import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
 
+const App = ()=>{
+    return(
+        <ChatEngine 
+        height = "100vh"
+        projectID = "35960980-665e-4c3f-b1eb-5fe098a410f4"
+        userName = "rameezqazi"
+        userSecret = "123456"
+
+        
+        />
+    );
+}
+
+export default App;
